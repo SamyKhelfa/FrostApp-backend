c4afdc4b77cd3964397f02f7d3d8806acba1ce39
samy <samy@gmail.com>
2024-01-04 19:25:18 +0100

    update Routes

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,7 +1,70 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
 
+// Route d'inscription
+router.post("/signup", async (req, res) => {
+  try {
+    const salt = uid2(16);
+    const hash = SHA256(req.body.password + salt).toString(encBase64);
+    const token = uid2(16);
+
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      role: req.body.role,
+      profilePicture: req.body.profilePicture,
+      program: req.body.program,
+      salt: salt,
+      hash: hash,
+      token: token,
+    });
+
+    await newUser.save();
+    res.status(201).json({
+      _id: newUser._id,
+      token: newUser.token,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      email: newUser.email,
+      // ... autres champs nécessaires
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Route de connexion
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const newHash = SHA256(req.body.password + user.salt).toString(encBase64);
+      if (newHash === user.hash) {
+        res.status(200).json({
+          _id: user._id,
+          token: user.token,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          // ... autres champs nécessaires
+        });
+      } else {
+        res.status(401).json({ message: "Mot de passe incorrect" });
+      }
+    } else {
+      res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Route pour récupérer tous les utilisateurs
 router.get("/", async (req, res) => {
   try {
     const users = await User.find();
@@ -11,16 +74,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
+// Route pour récupérer un utilisateur par son ID
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
@@ -34,6 +88,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// Route pour supprimer un utilisateur
 router.delete("/:id", async (req, res) => {
   try {
     const result = await User.findByIdAndDelete(req.params.id);
